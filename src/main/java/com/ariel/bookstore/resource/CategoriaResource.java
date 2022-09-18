320f2e11049cf28eb5a9f98cf557ffec8e9629f0
arielbat19 <arielbatista853@gmail.com>
2022-09-18 20:06:16 -0300

    POST para categoria

--- src/main/java/com/ariel/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/ariel/bookstore/resource/CategoriaResource.java
@@ -1,14 +1,20 @@
 package com.ariel.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ariel.bookstore.domain.Categoria;
 import com.ariel.bookstore.dtos.CategoriaDTO;
@@ -32,6 +38,13 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
