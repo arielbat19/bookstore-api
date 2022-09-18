440f2a98e6d7037565c9ddc5f31ddb8a26f53a4c
arielbat19 <arielbatista853@gmail.com>
2022-09-18 18:43:12 -0300

    findById para categoria

--- src/main/java/com/ariel/bookstore/domain/Livro.java
+++ src/main/java/com/ariel/bookstore/domain/Livro.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "livro")
 public class Livro implements Serializable{
@@ -26,6 +28,7 @@ public class Livro implements Serializable{
 	private String nomeAutor;
 	private String texto;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
